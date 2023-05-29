7c5f11e5a7a1712a1d242f69fcd5d25ac40836e2
bttobi <bttobi123@gmail.com>
2023-05-29 16:53:05 +0200

    created register form types

---
+++ src/data/errorRegisterTypes.ts
@@ -0,0 +1,16 @@
+const errorLoginTypes = [
+  {
+    name: "email",
+    type: "focus",
+  },
+  {
+    name: "password_register",
+    type: "focus",
+  },
+  {
+    name: "password_register_confirm",
+    type: "focus",
+  },
+];
+
+export default errorLoginTypes;
