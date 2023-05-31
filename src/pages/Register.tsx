3aaa774050d7b086062a3fe69dd436831ceecdbf
bttobi <bttobi123@gmail.com>
2023-05-31 21:16:13 +0200

    added prompts to register form page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,18 @@
+import { NavLink } from "react-router-dom";
 import RegisterForm from "../components/Forms/RegisterForm";
 
 const Register = () => {
-  return <RegisterForm />;
+  return (
+    <div className="align-center mt-16 flex flex-col items-center justify-center">
+      <RegisterForm />
+      <div className="align-center mt-16 flex flex-col items-center justify-center">
+        <p>Masz już konto?</p>
+        <NavLink to="/zaloguj" className="underline">
+          Zaloguj się tutaj
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
