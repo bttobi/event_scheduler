32ff419c9f7a19f290180eda3d0f45a24f9ff6f1
bttobi <bttobi123@gmail.com>
2023-05-31 21:16:05 +0200

    added some prompts to login form page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,20 @@
+import { NavLink } from "react-router-dom";
 import LoginForm from "../components/Forms/LoginForm";
 
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <div className="align-center mt-16 flex flex-col items-center justify-center">
+      <LoginForm />
+      <div className="align-center mt-16 flex flex-col items-center justify-center">
+        <p>Nie masz jeszcze konta?</p>
+        <NavLink to="/zarejestruj" className="underline">
+          Zarejestruj się tutaj
+        </NavLink>
+        <p className="mt-8">Zapomniałeś hasła?</p>
+        <button className="underline">Kliknij tutaj</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
