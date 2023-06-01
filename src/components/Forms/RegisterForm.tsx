0aec54b899c95a59b12611d210fd2a1c2b65a16c
bttobi <bttobi123@gmail.com>
2023-06-01 23:07:11 +0200

    register form styling

--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -23,21 +23,23 @@ const RegisterForm: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit((data) => login(data))}
-      className="align-center flex w-min flex-col items-center justify-center rounded-lg bg-blue-400"
+      className="align-center flex w-min flex-col items-center justify-center gap-2 rounded-lg bg-blue-400 px-10 py-4 pb-0 text-slate-400"
     >
-      <label>Email:</label>
-      <input
-        {...register("email_register", {
-          required: { value: true, message: "Pole jest wymagane" },
-        })}
-        type="email"
-        className="input"
-      />
-      <div className="relative h-4">
+      <div className="align-center relative flex items-center justify-center">
+        <label className="absolute">Email</label>
+        <input
+          {...register("email_register", {
+            required: { value: true, message: "Pole jest wymagane" },
+          })}
+          type="email"
+          className="input"
+        />
+      </div>
+      <div className="relative mb-4 h-4">
         <AnimatePresence>
           {errors?.email_register && (
             <motion.p
-              className="text-red-500"
+              className="font-bold text-red-500"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
@@ -47,19 +49,21 @@ const RegisterForm: React.FC = () => {
           )}
         </AnimatePresence>
       </div>
-      <label>Hasło:</label>
-      <input
-        {...register("password_register", {
-          required: { value: true, message: "Pole jest wymagane" },
-        })}
-        type="password"
-        className="input"
-      />
-      <div className="relative h-4">
+      <div className="align-center relative flex items-center justify-center">
+        <label className="absolute">Hasło</label>
+        <input
+          {...register("password_register", {
+            required: { value: true, message: "Pole jest wymagane" },
+          })}
+          type="password"
+          className="input"
+        />
+      </div>
+      <div className="relative mb-4 h-4">
         <AnimatePresence>
           {errors?.password_register && (
             <motion.p
-              className="text-red-500"
+              className="font-bold text-red-500"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
@@ -69,19 +73,21 @@ const RegisterForm: React.FC = () => {
           )}
         </AnimatePresence>
       </div>
-      <label>Potwierdź hasło:</label>
-      <input
-        {...register("password_register_confirm", {
-          required: { value: true, message: "Pole jest wymagane" },
-        })}
-        type="password"
-        className="input"
-      />
-      <div className="relative h-4">
+      <div className="align-center relative flex items-center justify-center">
+        <label className="absolute">Potwierdź hasło</label>
+        <input
+          {...register("password_register_confirm", {
+            required: { value: true, message: "Pole jest wymagane" },
+          })}
+          type="password"
+          className="input"
+        />
+      </div>
+      <div className="relative h-4 ">
         <AnimatePresence>
           {errors?.password_register_confirm && (
             <motion.p
-              className="text-red-500"
+              className="font-bold text-red-500"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
