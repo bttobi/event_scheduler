8f8f46c1660603e99c52380e54cd730442fb9799
bttobi <bttobi123@gmail.com>
2023-06-03 23:01:06 +0200

    added validation and animations to register form

--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -2,10 +2,16 @@ import { useForm } from "react-hook-form";
 import RegisterInputs from "../../types/RegisterInputs";
 import errorRegisterTypes from "../../data/errorRegisterTypes";
 import { AnimatePresence, motion } from "framer-motion";
+import { useRef } from "react";
 
 const RegisterForm: React.FC = () => {
+  const labelEmailRegisterRef = useRef<HTMLLabelElement>(null);
+  const labelPasswordRegisterRef = useRef<HTMLLabelElement>(null);
+  const labelPasswordConfirmRegisterRef = useRef<HTMLLabelElement>(null);
+
   const {
     register,
+    watch,
     handleSubmit,
     setError,
     formState: { errors },
@@ -23,16 +29,32 @@ const RegisterForm: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit((data) => login(data))}
-      className="align-center flex w-min flex-col items-center justify-center gap-2 rounded-lg bg-blue-400 px-10 py-4 pb-0 text-slate-400"
+      className="align-center flex w-min flex-col items-center justify-center gap-2 rounded-lg px-10 py-4 pb-0 text-slate-400"
+      noValidate
     >
       <div className="align-center relative flex items-center justify-center">
-        <label className="absolute">Email</label>
+        <label ref={labelEmailRegisterRef} className="label-default absolute">
+          Email
+        </label>
         <input
           {...register("email_register", {
             required: { value: true, message: "Pole jest wymagane" },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: "Wprowadź adres email",
+            },
+            onBlur: (e) => {
+              if (e.currentTarget.value == "")
+                labelEmailRegisterRef.current?.classList.remove("label-form");
+            },
+            onChange: () =>
+              labelEmailRegisterRef.current?.classList.add("label-form"),
           })}
           type="email"
           className="input"
+          onFocus={() =>
+            labelEmailRegisterRef.current?.classList.add("label-form")
+          }
         />
       </div>
       <div className="relative mb-4 h-4">
@@ -50,13 +72,29 @@ const RegisterForm: React.FC = () => {
         </AnimatePresence>
       </div>
       <div className="align-center relative flex items-center justify-center">
-        <label className="absolute">Hasło</label>
+        <label
+          ref={labelPasswordRegisterRef}
+          className="label-default absolute"
+        >
+          Hasło
+        </label>
         <input
           {...register("password_register", {
             required: { value: true, message: "Pole jest wymagane" },
+            onBlur: (e) => {
+              if (e.currentTarget.value == "")
+                labelPasswordRegisterRef.current?.classList.remove(
+                  "label-form"
+                );
+            },
+            onChange: () =>
+              labelPasswordRegisterRef.current?.classList.add("label-form"),
           })}
           type="password"
           className="input"
+          onFocus={() =>
+            labelPasswordRegisterRef.current?.classList.add("label-form")
+          }
         />
       </div>
       <div className="relative mb-4 h-4">
@@ -74,13 +112,36 @@ const RegisterForm: React.FC = () => {
         </AnimatePresence>
       </div>
       <div className="align-center relative flex items-center justify-center">
-        <label className="absolute">Potwierdź hasło</label>
+        <label
+          ref={labelPasswordConfirmRegisterRef}
+          className="label-default absolute"
+        >
+          Powtórz hasło
+        </label>
         <input
           {...register("password_register_confirm", {
             required: { value: true, message: "Pole jest wymagane" },
+            validate: (val: string) => {
+              if (watch("password_register") != val) {
+                return "Hasła nie są zgodne";
+              }
+            },
+            onBlur: (e) => {
+              if (e.currentTarget.value == "")
+                labelPasswordConfirmRegisterRef.current?.classList.remove(
+                  "label-form"
+                );
+            },
+            onChange: () =>
+              labelPasswordConfirmRegisterRef.current?.classList.add(
+                "label-form"
+              ),
           })}
           type="password"
           className="input"
+          onFocus={() =>
+            labelPasswordConfirmRegisterRef.current?.classList.add("label-form")
+          }
         />
       </div>
       <div className="relative h-4 ">
