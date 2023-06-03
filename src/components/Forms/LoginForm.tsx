bcf5e1c263fe2892f99fa416e017d06ebdf87a77
bttobi <bttobi123@gmail.com>
2023-06-03 23:00:59 +0200

    added validation and animations to login form

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,9 +1,13 @@
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import LoginInputs from "../../types/LoginInputs";
 import errorLoginTypes from "../../data/errorLoginTypes";
-import { AnimatePresence, motion } from "framer-motion";
 
 const LoginForm: React.FC = () => {
+  const labelEmailLoginRef = useRef<HTMLLabelElement>(null);
+  const labelPasswordLoginRef = useRef<HTMLLabelElement>(null);
+
   const {
     register,
     handleSubmit,
@@ -23,16 +27,32 @@ const LoginForm: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit((data) => login(data))}
-      className="align-center flex w-min flex-col items-center justify-center gap-2 rounded-lg bg-blue-400 px-10 py-4 pb-0 text-slate-400"
+      className="align-center flex w-min flex-col items-center justify-center gap-2 rounded-lg px-10 py-4 pb-0 text-slate-400"
+      noValidate
     >
       <div className="align-center relative flex items-center justify-center">
-        <label className="absolute">Email</label>
+        <label ref={labelEmailLoginRef} className="label-default absolute">
+          Email
+        </label>
         <input
           {...register("email_login", {
             required: { value: true, message: "Pole jest wymagane" },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: "Wprowadź adres email",
+            },
+            onBlur: (e) => {
+              if (e.currentTarget.value == "")
+                labelEmailLoginRef.current?.classList.remove("label-form");
+            },
+            onChange: () =>
+              labelEmailLoginRef.current?.classList.add("label-form"),
           })}
           type="email"
           className="input"
+          onFocus={() =>
+            labelEmailLoginRef.current?.classList.add("label-form")
+          }
         />
       </div>
       <div className="relative mb-4 h-4">
@@ -50,13 +70,24 @@ const LoginForm: React.FC = () => {
         </AnimatePresence>
       </div>
       <div className="align-center relative flex items-center justify-center">
-        <label className="absolute">Hasło</label>
+        <label ref={labelPasswordLoginRef} className="label-default absolute">
+          Hasło
+        </label>
         <input
           {...register("password_login", {
             required: { value: true, message: "Pole jest wymagane" },
+            onBlur: (e) => {
+              if (e.currentTarget.value == "")
+                labelPasswordLoginRef.current?.classList.remove("label-form");
+            },
+            onChange: () =>
+              labelPasswordLoginRef.current?.classList.add("label-form"),
           })}
           type="password"
           className="input"
+          onFocus={() =>
+            labelPasswordLoginRef.current?.classList.add("label-form")
+          }
         />
       </div>
       <div className="relative h-4">
