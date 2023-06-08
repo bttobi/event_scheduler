6a90f99efcd455f9f0f1c2b8018b1c1f35784249
bttobi <bttobi123@gmail.com>
2023-06-08 18:47:18 +0200

    Update BookedDates.tsx

--- src/pages/BookedDates.tsx
+++ src/pages/BookedDates.tsx
@@ -1,5 +1,9 @@
 const BookedDates = () => {
-  return <div>BookedDates</div>;
+  return (
+    <div className="align-center mt-12 flex h-full w-full flex-col items-center justify-center">
+      <div className="text-2xl">Moje terminy (ostatnie 30 dni):</div>
+    </div>
+  );
 };
 
 export default BookedDates;
