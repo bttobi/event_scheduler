e30b4c36068e70073b2df22acc9e10529232f1c9
bttobi <bttobi123@gmail.com>
2023-06-08 18:45:53 +0200

    created global user context

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,26 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/UI/Navbar";
 import BookedDates from "./pages/BookedDates";
+import { createContext } from "react";
+import { auth } from "./firebase.js";
+
+export const UserContext = createContext(auth);
 
 const App: React.FC = () => {
   return (
-    <main className="font-article">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/zaloguj" element={<Login />} />
-          <Route path="/zarejestruj" element={<Register />} />
-          <Route path="/terminy" element={<BookedDates />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <UserContext.Provider value={auth}>
+      <main className="font-article">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/zaloguj" element={<Login />} />
+            <Route path="/zarejestruj" element={<Register />} />
+            <Route path="/terminy" element={<BookedDates />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </UserContext.Provider>
   );
 };
 
