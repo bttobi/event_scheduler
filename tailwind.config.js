84a2f9c88b2700360e953b61b429e69a7aac4cd6
bttobi <bttobi123@gmail.com>
2023-06-08 17:41:38 +0200

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  theme: {
+    extend: {
+      fontFamily: {
+        header: ["Montserrat", "sans-serif"],
+        article: ["Lato", "sans-serif"],
+      },
+    },
+  },
 };
