04d66be6998f02c41a5ebaeeb829a06a6777aff0
bttobi <bttobi123@gmail.com>
2023-06-09 21:42:07 +0200

    now using the alert provider and its context

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,28 @@ import Navbar from "./components/UI/Navbar";
 import BookedDates from "./pages/BookedDates";
 import { createContext } from "react";
 import { auth } from "./firebase.js";
+import { AlertProvider } from "./contexts/AlertProvider.js";
+import Alert from "./components/UI/Alert.js";
 
 export const UserContext = createContext(auth);
 
 const App: React.FC = () => {
   return (
     <UserContext.Provider value={auth}>
-      <main className="font-article">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/zaloguj" element={<Login />} />
-            <Route path="/zarejestruj" element={<Register />} />
-            <Route path="/terminy" element={<BookedDates />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
+      <AlertProvider>
+        <main className="font-article">
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/zaloguj" element={<Login />} />
+              <Route path="/zarejestruj" element={<Register />} />
+              <Route path="/terminy" element={<BookedDates />} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+        <Alert />
+      </AlertProvider>
     </UserContext.Provider>
   );
 };
