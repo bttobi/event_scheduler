05d4994ba70024b8fd2f3bbd5fede33eca6b284d
bttobi <bttobi123@gmail.com>
2023-06-25 23:42:20 +0200

    Update RegisterForm.tsx

--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -89,6 +89,8 @@ const RegisterForm: React.FC = () => {
             labelEmailRegisterRef.current?.classList.add("label-form")
           }
         />
+        // TODO NAME, SURNAME, CHECKBOX WHETHER BLACK GUNPOWDER, // NUMBER OF
+        LICENSE
       </div>
       <div className="relative mb-4 h-4">
         <AnimatePresence>
