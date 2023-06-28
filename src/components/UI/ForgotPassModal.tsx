cdc9fb4824e97af38894cda9d209c240fc1e3cdb
bttobi <bttobi123@gmail.com>
2023-06-28 17:52:58 +0200

    Create ForgotPassModal.tsx

---
+++ src/components/UI/ForgotPassModal.tsx
@@ -0,0 +1,22 @@
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const ForgotPassModal: React.FC = () => {
+  return (
+    <label htmlFor="my-modal-7" className="modal cursor-pointer">
+      <label className="align-center modal-box flex flex-col items-center justify-center">
+        <div className="absolute right-2 top-2">
+          <label
+            htmlFor="my-modal-7"
+            className="align-center btn-sm btn flex  items-center justify-center bg-red-500 text-white transition-all hover:bg-red-300"
+          >
+            <AiFillCloseCircle size={20} />
+          </label>
+        </div>
+        <p className="text-xl font-bold">Na pewno zresetować hasło?</p>
+        <button>Wyślij email z resetem</button>
+      </label>
+    </label>
+  );
+};
+
+export default ForgotPassModal;
