c33aa149418be35f9168666308b99c4ec6f6f050
bttobi <bttobi123@gmail.com>
2023-07-03 23:42:18 +0200

    Update ForgotPassModal.tsx

--- src/components/UI/ForgotPassModal.tsx
+++ src/components/UI/ForgotPassModal.tsx
@@ -13,7 +13,7 @@ const ForgotPassModal: React.FC = () => {
           </label>
         </div>
         <p className="text-xl font-bold">Na pewno zresetować hasło?</p>
-        <button className="btn-success btn h-16">Wyślij email z resetem</button>
+        <button className="btn-success btn h-12">Wyślij email z resetem</button>
       </label>
     </label>
   );
